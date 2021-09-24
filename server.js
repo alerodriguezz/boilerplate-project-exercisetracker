a23c4c0cbab05c274dae814a9a5cfc25e74c32a3
arod <alerodriguez@csumb.edu>
2021-09-24 08:04:23 +0000

    initialized mongodb connection and fulfilled first test case

--- server.js
+++ server.js
@@ -1,18 +1,40 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const { Schema } = mongoose;
 require('dotenv').config()
 
+//connect to db 
+mongoose.connect(
+ process.env['MONGO_URL']
+|| 'mongodb://localhost/exercise-track')
+
+const userSchema = new Schema({ username:String });
+const User = mongoose.model('user', userSchema);
+
 app.use(cors())
+
+app.use(bodyParser.urlencoded({entended:false}))
+app.use(bodyParser.json())
+
 app.use(express.static('public'))
+
+//landing page 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/users',(req,res)=> {
+const newUser = new User({username: req.body.username});
+newUser.save((err,data) => {
+  res.json({username: data.username, "_id":data.id})
+})
 
-
+});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
-})
+});
