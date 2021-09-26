f4fe1fde53c56f91d16b6992b5a9db72b010976b
arod <alerodriguez@csumb.edu>
2021-09-26 23:28:09 +0000

    Posting exercise function tests now pass

--- server.js
+++ server.js
@@ -11,9 +11,19 @@ mongoose.connect(
  process.env['MONGO_URL']
 || 'mongodb://localhost/exercise-track')
 
-const userSchema = new Schema({ username:String });
+//database structure 
+const userSchema = new Schema({ username:{type: String, unique: true} });
 const User = mongoose.model('user', userSchema);
 
+const exerciseSchema = new Schema({
+  userId: String ,
+  description: String,
+  duration: Number ,
+  date: Date
+})
+const Exercise = mongoose.model('Exercise',exerciseSchema)
+
+//middleware
 app.use(cors())
 
 app.use(bodyParser.urlencoded({entended:false}))
@@ -26,15 +36,47 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+
+
 app.post('/api/users',(req,res)=> {
 const newUser = new User({username: req.body.username});
 newUser.save((err,data) => {
+  if(err){
+    res.json("Username already taken")
+  }
+  else{
   res.json({username: data.username, "_id":data.id})
+  }
 })
 
 });
 
+app.post('/api/users/:_id/exercises', (req,res) => {
+  const id = req.params._id
+  const {description,duration,date} = req.body;
+  console.log(id)
+  User.findById(id,(err,data)=> {
+    if(!data){
+      res.send("Cast to ObjectId failed for value "+ id+" at path _id for model User")
+    }
+    else {
+      const username = data.username;
+
+        if (date == '') {
+                let today = new Date()
+                date = today.toDateString()
+            }
+  const newExercise = new Exercise ({id, description,duration,date})
+  newExercise.save((err,data)=>{
+        res.json({username, description, duration:Number(duration), date: new Date(date).toDateString(), _id:id})
+     })
+    }
+      })
+
+
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 });
+
